c366d88915bfb4b32007488ad6b38c13a76d6086
dvizhok <kalibry1@gmail.com>
2025-09-13 21:38:34 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,14 @@
-// function makeTransaction(quantity, pricePerDroid) {
-//   return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
-// }
-
-// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-// const x = prompt('Enter quantity');
-// const y = prompt('Enter price');
-// console.log(makeTransaction(x, y));
-
-function checkAge(age) {
-  let x;
-  if (age >= 18) {
-    x = 'You are an adult';
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const summ = quantity * pricePerDroid;
+  if (summ > customerCredits) {
+    return 'Insufficient funds!';
+  } else {
+    return `You ordered ${quantity} droids worth ${summ} credits!`;
   }
-  return x;
 }
-console.log(checkAge(15));
+
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
